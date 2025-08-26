08b775e1b9bd3d548f707510d6316a6b20be79d7
HopperAlex04 <hopperalex04@gmail.com>
2025-08-26 07:43:16 -0400

    Added initial classes necessary for creating game states.

---
+++ Cuttle.py
@@ -0,0 +1,6 @@
+# The game itself, when this file is run it should present options for playing against the algorithims or yourself.
+
+# TODO: Gamestates
+# TODO: Turns
+# TODO: Hands
+# TODO: Card Functionality
