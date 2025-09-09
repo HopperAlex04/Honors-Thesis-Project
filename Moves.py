bb78da29c5ec805e7fb1650b9e6bad578abd3324
HopperAlex04 <hopperalex04@gmail.com>
2025-09-09 19:36:55 -0400

    Added ScuttlePlay constructor

--- Moves.py
+++ Moves.py
@@ -18,3 +18,11 @@ class ScorePlay():
         self.field.cards.append(self.hand.cards.pop(self.hand.cards.index(self.card)))
         #print(self.hand.cards)
         #print(self.field.cards)
+        
+class ScuttlePlay():
+    def __init__(self, card:Card, target:Card, hand:Hand, field:Zone, scrap:Zone):
+        self.card = card
+        self.target = target
+        self.field = field
+        self.hand = hand
+        self.scrap = scrap
