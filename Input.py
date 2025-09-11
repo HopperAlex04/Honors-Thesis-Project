e1f73d69a0ed94ebdab24415cc46fdf73b39c416
HopperAlex04 <hopperalex04@gmail.com>
2025-09-11 11:58:17 -0400

    Added Input.py and determined classes for player control

---
+++ Input.py
@@ -0,0 +1,13 @@
+from Person import Player
+
+
+class Manual():
+    def __init__(self, position:Player) -> None:
+        pass
+
+
+
+
+class Agent():
+    def __init__(self, position:Player) -> None:
+        pass
