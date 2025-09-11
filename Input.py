e5ad3dc67f7cbca60bdb5591d8f9eea6fd9ba7ca
HopperAlex04 <hopperalex04@gmail.com>
2025-09-11 12:12:02 -0400

    added some notes for later

--- Input.py
+++ Input.py
@@ -1,13 +1,14 @@
 from Person import Player
+from Zone import Hand
 
 
 class Manual(Player):
-    def __init__(self) -> None:
+    def __init__(self, hand: Hand, name: str) -> None:
         pass
 
 
 
 
 class Automatic(Player):
-    def __init__(self) -> None:
+    def __init__(self, hand: Hand, name: str) -> None:
         pass
