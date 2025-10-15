620c0e0eae72e714e2d0f3cc5fe1496d71cf8af4
HopperAlex04 <hopperalex04@gmail.com>
2025-10-15 11:54:54 -0400

    Established framework for an external gameloop for training

--- Training.py
+++ Training.py
@@ -15,12 +15,14 @@ def trainingLoop(episodes):
             action = 0
             ob, score, terminated = env.step(action)
             #This could probably be part of the step function
+            if terminated:break
             env.passControl()
             #Get a reward based on the data from step and feed the training alg
             
             #Get an action from the model
             action = 0
             ob, score, terminated = env.step(action)
+            if terminated:break
             #This could probably be part of the step function
             env.passControl()
             #Get a reward based on the data from step and feed the training alg
