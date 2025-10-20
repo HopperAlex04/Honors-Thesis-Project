338b6cd65aba03e3219474221775c870541fa6f8
HopperAlex04 <hopperalex04@gmail.com>
2025-10-20 14:31:25 -0400

    Removed unnecessary Player class, getting actions will be a helper method

--- Training.py
+++ Training.py
@@ -4,21 +4,9 @@ import random
 from GameEnvironment import CuttleEnvironment
 
 
-class Player(ABC):
+class Randomized():
     def __init__(self, name):
         self.name = name
-    
-    @abstractmethod
-    def getAction(self, validActions):
-        pass
-    
-    def getReward(self, ob, score, terminated):
-        if terminated and score >= 21: return 1
-
-
-class Randomized(Player):
-    def __init__(self, name):
-        super().__init__(name)
 
     def getAction(self, validActions:list) -> int:
         return validActions[random.randint(0, validActions.__len__() - 1)]
