1f0ff4ecd22b3d64fc7849c42513a0629e8a7030
HopperAlex04 <hopperav@appstate.edu>
2025-11-07 12:37:16 -0500

    Added loop into main.py for training experiments

--- Main.py
+++ Main.py
@@ -1,4 +1,39 @@
-import NetworkTesting
-import EnvTests
+import torch
+from GameEnvironment import CuttleEnvironment
+from Networks import NeuralNetwork
+import Players
+from Training import WinRewardTraining
 
-NetworkTesting.trainingTest()
+
+input("Press enter to begin training:")
+
+user_ended = False
+
+env = CuttleEnvironment()
+actions = env.actions
+model = NeuralNetwork(260, actions, None)
+
+#Eventually make these adjustable as well
+BATCH_SIZE = 8192
+GAMMA = 0.7
+EPS_START = 0.9
+EPS_END = 0.01
+EPS_DECAY = 2500
+TAU = 0.005
+LR = 3e-4
+trainee = Players.Agent("Agent01", model, BATCH_SIZE, GAMMA, EPS_START, EPS_END, EPS_DECAY, TAU, LR )
+validation01 = Players.HueristicHighCard("HighCard")
+
+t = WinRewardTraining(trainee, trainee)
+
+while not user_ended:
+    user_in = input("Enter 't' to train,'v' to validate,  or 's' to save")
+    match user_in:
+        case "t":
+            t.trainLoop(500)
+        case "v":
+            t.validLoop(trainee, validation01, True, 500)
+        case "s":
+            model_name = input("Name the model")
+            torch.save(trainee.model, f"models/{model_name}")
+
